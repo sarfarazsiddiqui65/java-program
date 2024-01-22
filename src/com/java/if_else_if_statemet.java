0b466f334cca3ac57aafeccb220fd65ceda41ea0
sarfarazsiddiqui65 <siddiquisarfarazahmad65@gmail.com>
2024-01-22 06:04:10 -0800

    if_else_program

--- src/com/java/if_else_if_statemet.java
+++ src/com/java/if_else_if_statemet.java
@@ -7,19 +7,20 @@ public class if_else_if_statemet {
 	public static void main(String[] args) {
     int marks;
     System.out.println("enter number");
-    Scanner sc=new Scanner(System.in);
-    marks=sc.nextInt();
+    Scanner Sc=new Scanner(System.in);
+    marks=Sc.nextInt();
     if(marks>80)
-     {
-    	System.out.println("toper");
-     }
-     else if (marks<80 && marks>=60)
-     {
+    {
+    	System.out.println("topper");
+    }
+    else if(marks <=80 && marks >= 60)
+    {
     	System.out.println("middle");
-     }
-     else
-     {
-    	System.out.println("lower"); 
-     }  
+    }
+    else
+    {
+    	System.out.println("lower");
+    }
+
 	}
 }
