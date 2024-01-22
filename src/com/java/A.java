f8329f4bcfefcb8c430234b844cf6861d5d8d2d0
sarfarazsiddiqui65 <siddiquisarfarazahmad65@gmail.com>
2024-01-22 01:44:49 -0800

    sarfaraz

---
+++ src/com/java/A.java
@@ -0,0 +1,22 @@
+package com.java;
+
+class A {
+	int a; String b; boolean c;
+      A()
+      {
+    	  a=10; b= "ankush"; c=true; 
+      }
+      void disp()
+      {
+    	  System.out.println(a+" "+b+ " "+c);
+      }
+	public static void main(String[] args) {
+    
+	   A a=new A();
+	   a.disp();
+		
+
+
+	}
+
+}
