bb7a9660cc06957c4118fe139eb15d514ce46049
sarfarazsiddiqui65 <siddiquisarfarazahmad65@gmail.com>
2024-01-22 04:55:46 -0800

    if_else_program

---
+++ src/com/java/If_else_statement.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class If_else_statement {
+
+	public static void main(String[] args) {
+	int n;
+	System.out.println("enter any number");
+	Scanner Sc=new Scanner(System.in);
+	n=Sc.nextInt();
+	if(n>=0)
+	{
+		System.out.println("+ve number");
+	}
+	else
+	{
+		System.out.println("-ve number");
+	}
+	
+	
+	
+	
+	
+	
+	}
+	
+}
