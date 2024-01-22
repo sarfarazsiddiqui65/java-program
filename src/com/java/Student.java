f6eb8485567e19d372e92980fc8661c51c57aad8
sarfarazsiddiqui65 <siddiquisarfarazahmad65@gmail.com>
2024-01-21 23:44:05 -0800

    name

--- src/com/java/Student.java
+++ src/com/java/Student.java
@@ -4,19 +4,21 @@ public class Student {
 	private int id;
 	private String name;
 	private String email;
+	private long phone;
 	
-	Student(int id,String name, String email){
+	Student(int id,String name, String email, long phone){
 		this.id=id;
 		this.name=name;
 		this.email=email;
+		this.phone=phone;
 	}
 
 	void show(){
-		System.out.println(id + " " + name +" " + email);
+		System.out.println(id + " " + name +" " + email+" "+phone);
 	}
 	public static void main(String[] args) {
     
-    Student student = new Student(1, "Aamir","sarfaraz@gamil.com");
+    Student student = new Student(1, "Aamir","sarfaraz@gamil.com",1224353522);
     student.show();
     
 
